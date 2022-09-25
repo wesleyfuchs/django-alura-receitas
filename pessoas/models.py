00f11fcc537654cf74be4bd814d08822e835e731
uesleyfucks <fuchswesley@hotmail.com>
2022-09-25 19:22:45 -0300

    Criando Pessoa e vinculando com Receita

---
+++ pessoas/models.py
@@ -0,0 +1,8 @@
+import email
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nome
