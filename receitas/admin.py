f7b5b86014b83c8593285e39173c7bcc88332af8
uesleyfucks <fuchswesley@hotmail.com>
2022-09-26 19:20:57 -0300

    carregando media

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria')
+    list_display = ('id', 'nome_receita', 'categoria', 'publicada')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita', )
     list_filter = ('categoria', )
+    list_editable = ('publicada', )
     list_per_page = 10
 
 
--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from datetime import datetime
 from pessoas.models import Pessoa
@@ -11,4 +12,5 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)    
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
-    
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
+    publicada = models.BooleanField(default=False)
