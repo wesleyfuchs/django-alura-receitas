0d83a3c6caec9a9722f3757951083d95b4994894
uesleyfucks <fuchswesley@hotmail.com>
2022-09-27 21:32:02 -0300

    criando app usuarios

---
+++ usuarios/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def cadastro(request):
+    return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    return render(request, 'usuarios/login.html')
+
+def dashboard(request):
+    pass
+
+def logout(request):
+    pass
+
