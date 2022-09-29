e2ca71231eebdebb709c4cc71ac4aa1a857dbda7
uesleyfucks <fuchswesley@hotmail.com>
2022-09-28 22:54:32 -0300

    modularizacao das views

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -1,6 +1,6 @@
 from gc import get_objects
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
-from .models import Receita
+from receitas.models import Receita
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 
@@ -22,20 +22,6 @@ def receita(request, receita_id):
     }
     
     return render(request ,'receitas/receita.html', receita_a_exibir)
-
-def buscar(request):
-    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
-    
-    if 'buscar' in request.GET:
-        nome_a_buscar = request.GET['buscar']
-        if buscar:
-            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
-            
-    dados = {
-        'receitas': lista_receitas
-    }
-    
-    return render(request, 'receitas/buscar.html', dados)
 
 def cria_receita(request):
     if request.method == 'POST':
